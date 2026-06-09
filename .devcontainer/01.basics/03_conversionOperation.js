f0161ce4f81e8e5a432ad105537c45b60df9e4cc
darshanthelucifer-png <darshanthelucifer@gmail.com>
2026-06-09 07:35:42 +0000

    conversion of datatype

---
+++ .devcontainer/01.basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "100abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" ==> 33
+//"33abc" ==>Nan(not a number)
+// true =>1; false =>0
+
+let isloggedIn = "dd"
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"darshan"==> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

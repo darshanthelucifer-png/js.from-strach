f1b9ae98a45859fe96178e5ae846191b058b20be
darshanthelucifer-png <darshanthelucifer@gmail.com>
2026-06-10 09:10:57 +0000

    comparision

---
+++ javascript/01.basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2 <= 1);
+// console.log(2!=1);
+// console.log(2==2);
+
+
+// console.log("02">1);
+// console.log("2">1);
+
+// console.log(null > 0); avoid this type of code
+// console.log(null==0); avoid this type of code
+// console.log(null>=0); avoid this type of code
+
+// console.log(undefined==0); avoid this type of code
+// console.log(undefined>=0); avoid this type of code
+// console.log(undefined<0); avoid this type of code
+
+//=== strict comparision
+
+// console.log("2"==2); it converting string to number ==
+
+// console.log("2"===2);   === strict comparision
+
+
+
